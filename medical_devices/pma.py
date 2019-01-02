1a9fa3c4428ed3dca4767ca39776709d7f1f5d91
Caleb Ho <caleb.yh.ho@gmail.com>
2019-01-02 13:22:33 -0500

    Cast date and boolean types in PMA

--- medical_devices/pma.py
+++ medical_devices/pma.py
@@ -4,13 +4,16 @@ import requests
 import os
 import zipfile
 
-from typing import List, Dict
+from datetime import datetime
+from typing import List, Dict, Optional, Union
 
 PMA_DATA_DIR = 'data/pma/'
 PMA_URI = 'http://www.accessdata.fda.gov/premarket/ftparea/pma.zip'
 
+TVal = Optional[Union[str, bool, datetime]]
 
-def get_pma() -> List[Dict[str, str]]:
+
+def get_pma() -> List[Dict[str, TVal]]:
     if not os.path.exists(PMA_DATA_DIR):
         os.makedirs(PMA_DATA_DIR, mode=0o755)
     fname = os.path.join(PMA_DATA_DIR, 'pma.txt')
@@ -25,11 +28,23 @@ def get_pma() -> List[Dict[str, str]]:
         headers = f.readline().strip().split('|')
         for line in f:
             values = line.strip().split('|')
-            devices.append(dict(zip(headers, values)))
+            device: Dict[str, TVal] = {}
+            for header, value in zip(headers, values):
+                if value == '':
+                    device[header] = None
+                elif value in {'Y', 'N'}:
+                    device[header] = True if value == 'Y' else False
+                else:
+                    try:
+                        device[header] = datetime.strptime(value, '%m/%d/%Y')
+                    except ValueError:
+                        device[header] = value
+            devices.append(device)
     return devices
 
 
 if __name__ == '__main__':
+    from pprint import pprint
     devices = get_pma()
     print(len(devices))
-    print(devices[0])
+    pprint(devices[0])
