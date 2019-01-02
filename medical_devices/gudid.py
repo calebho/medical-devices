fe246f6e9670d8725d0935423b663c6bc16b18fb
Caleb Ho <caleb.yh.ho@gmail.com>
2019-01-02 14:16:07 -0500

    Download new data if available

--- medical_devices/gudid.py
+++ medical_devices/gudid.py
@@ -23,7 +23,8 @@ async def gen_gudid() -> AsyncIterator[Dict[str, TVal]]:
     async with ClientSession(timeout=ClientTimeout(None)) as session:
         await gen_gudid_data(session)
 
-    fname = os.path.join(GUDID_DATA_DIR, DEVICE_DATA_FNAME)
+    fname = os.path.join(GUDID_DATA_DIR, get_gudid_data_dir(),
+                         DEVICE_DATA_FNAME)
     with open(fname) as f:
         headers = f.readline().strip().split('|')
         for line in f:
@@ -43,7 +44,8 @@ async def gen_gudid() -> AsyncIterator[Dict[str, TVal]]:
 
 
 async def gen_gudid_data(session: ClientSession) -> None:
-    if os.path.exists(os.path.join(GUDID_DATA_DIR, DEVICE_DATA_FNAME)):
+    dirname = os.path.join(GUDID_DATA_DIR, get_gudid_data_dir())
+    if os.path.exists(dirname):
         return
 
     uri = get_gudid_data_uri()
@@ -51,10 +53,15 @@ async def gen_gudid_data(session: ClientSession) -> None:
     buf = io.BytesIO()
     async for b in r.content.iter_any():
         buf.write(b)
+    os.makedirs(dirname)
     with zipfile.ZipFile(buf) as z:
-        z.extractall(path=GUDID_DATA_DIR)
+        z.extractall(path=dirname)
 
 
 def get_gudid_data_uri() -> str:
+    return f'https://accessgudid.nlm.nih.gov/release_files/download/{get_gudid_data_dir()}.zip'
+
+
+def get_gudid_data_dir() -> str:
     date_str = time.strftime('%Y%m') + '01'
-    return f'https://accessgudid.nlm.nih.gov/release_files/download/AccessGUDID_Delimited_Full_Release_{date_str}.zip'
+    return f'AccessGUDID_Delimited_Full_Release_{date_str}'
